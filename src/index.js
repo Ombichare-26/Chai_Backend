7a6bee53f4d06dfd60712c0b874835df856f0a42
Ombichare@26 <ombichare2006@gmail.com>
2026-01-06 13:41:05 +0530

    add mongoDB atlass

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import dotenv from 'dotenv'
+import connectDB from './db/index.js'
+
+dotenv.config({
+    path:'./env'
+})
+connectDB();
+
+/*
+
+1st Approach:
+
+import express from 'express'
+
+const app = express();
+
+// IIFE - Immediately Invoked Function Expression.
+(async () => {
+
+    try {
+      await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_name}`)
+        app.on("error",()=>{
+
+            console.log("ERROR: ",error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT,()=>{
+
+            console.log(`App is listening on Port: ${process.env.PORT}`);
+            
+        })
+    } catch (error) {
+        console.error("Error: ",error);
+
+        throw error;
+    }
+})()
+*/
