eca09ab3eece105f69da66b042aa4b43c81ed915
Ombichare@26 <ombichare2006@gmail.com>
2026-01-15 22:01:02 +0530

    solved small mistake

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -39,7 +39,7 @@ router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
 
-router.route("/coverImage").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+router.route("/coverImage").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 
